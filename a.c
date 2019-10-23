c15fa2254fbe2fd7cf0c26036861960e64f13ae0
Dylan wang shuai <21750188@student.uwa.edu.au>
2019-10-23 13:02:19 +0800

    result update

--- a.c
+++ a.c
@@ -12,6 +12,7 @@ int **Mat;
 int **output;
 int **final;
 
+//read matrix from binary format
 void fileread(char *file){
   int rownum;
   int colnum;
@@ -34,20 +35,17 @@ void fileread(char *file){
     for(colnum=0; colnum<row_col[0]; colnum++){
       fread(token,sizeof(token),1,fp);
       Mat[rownum][colnum] = token[0];
-
     }
-
   }
-
 }
 
+//implemented Floyd Warshal algorism finding the shortest path
 void getshortpath(){
   int row;
   int col;
   int a;
   int my_rank;
   int size;
-  int count;
   int point;
   MPI_Request request;
   MPI_Status status;
@@ -55,35 +53,34 @@ void getshortpath(){
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
+  // allocate memory for 2D array
   output = (int **)malloc(row_col[0] * sizeof(int*));
   for (a=0; a<row_col[0]; a++){
       output[a] = (int *)malloc( row_col[0] * sizeof(int));
   }
 
-
+ //allocate memory for result 2D array
   final =  (int **)malloc(row_col[0] * sizeof(int*));
   for (a=0; a<row_col[0]; a++){
       final[a] = (int *)malloc( row_col[0] * sizeof(int));
   }
-
-
-
+    //get chunk size based on rows
     int chunk_size = row_col[0]/(size);
-      int previous;
-      int until;
-      int remind;
-      count = my_rank;
+    int remind; //chunk size for reminder node (last node)
+    int previous; //each node's first row number
+    int until; //each node's first row number
 
+      //divide up tasks
       if(my_rank == (size-1)){
-         previous = count * chunk_size;
+         previous = my_rank * chunk_size;
          until = row_col[0];
          remind = until - previous;
       }
       else{
-       previous = count * chunk_size;
+       previous = my_rank * chunk_size;
        until = previous + chunk_size;
       }
-
+      //algorism based on each node
       for(row=previous; row<until;row++){
         for(col=0;col<row_col[0];col++){
           if(row==col){
@@ -105,23 +102,19 @@ void getshortpath(){
         }
       }
 
-
+    //send back results to last node row by row
     for(point=0; point<chunk_size;point++){
     if(my_rank==(size-1)){
       int r;
       for(r=(size-2); r>=0; r--){
-    //  for(col=0; col<row_col[0];col++){
       MPI_Recv(output[r*chunk_size+point], row_col[0], MPI_INT, r, 0,MPI_COMM_WORLD,&status);
-  //  }
     }
     }
     else{
-    // for(col=0;col<row_col[0];col++){
       MPI_Send(output[previous+point],row_col[0], MPI_INT, size-1, 0, MPI_COMM_WORLD);
-    //}
     }
     }
-
+        //testing result
         /*if(my_rank==(size-1)){
           int b;
         for(a=0;a<row_col[0];a++){
@@ -133,10 +126,7 @@ void getshortpath(){
       }*/
 }
 
-
-
-
-
+//write file function
 void filewrite(char *f){
   char *out;
   char outname[1000];
@@ -160,16 +150,17 @@ void filewrite(char *f){
 
 
 int main(int argc, char** argv) {
-        MPI_Init(&argc, &argv);
         int rank;
         int sizea;
         int opt;
         int fflag = 0, errflag =0;
         int **out;
         char filename[1000];
+        MPI_Init(&argc, &argv);
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
         MPI_Comm_size(MPI_COMM_WORLD, &sizea);
         struct timeval start, end;
+        //check how many argument
         if(argc > 3){
           printf("too many arguments were taken, please check again\n");
           return -1;
@@ -178,7 +169,7 @@ int main(int argc, char** argv) {
           printf("Too few arguments were taken, please use -f filename or filename to compile\n");
           return -1;
         }
-
+        //getopt to assign flag
         while ((opt = getopt (argc, argv, "f:")) != -1){
            switch (opt){
              case 'f':
@@ -201,17 +192,14 @@ int main(int argc, char** argv) {
         gettimeofday(&start, NULL);
         getshortpath();
         gettimeofday(&end, NULL);
+        //get time taken
         double delta = ((end.tv_sec - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
-
 
         if(rank==(sizea-1)){
         filewrite(filename);
         printf("time elapsed = %12.10f on rank %d\n", delta, rank);
+        //only print the time taken for last node(as it's the longgest)
         }
         MPI_Finalize();
-
-
-
         return 0;
-
 }
